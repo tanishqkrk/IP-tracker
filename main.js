e70c06f5c5e980a35a3a41fd259de57f89dd7118
tanishqkrk <tanishqkrk@gmail.com>
2022-08-19 14:55:52 +0530

    some changes

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-let map = L.map('map').setView([0, 0], 0);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19
+const apiKey = 'at_Afae2z1wztB5f4gRmhht2iZPnyeG3';
+const currentIp = document.querySelector('.ip__value');
+const currentLocation = document.querySelector('.location__value');
+const currentTimeZone = document.querySelector('.timezone__value');
+const currentIsp = document.querySelector('.isp__value');
+let map = new L.map('map');
+let x = 0;
+let y = 0;
+let z = 1;
+const marker = L.marker([x, y], z);
+const enteredIp = document.querySelector('.header__input--input');
+const submitButton = document.querySelector('.header__input--button');
+
+map.setView([x, y], z);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-let marker = L.marker([0, 0], 0).addTo(map);
-
-function update(e) {
-  document.querySelector('.ip__value').innerHTML = document.querySelector('.header__input--input').value
-
-}
-
-let submit = document.querySelector('.header__input--button');
-submit.addEventListener('click', update)
+submitButton.addEventListener('click', e => {
+    marker.addTo(map).bindPopup(`${enteredIp.value}<br>${currentLocation.innerText}`);
+})
