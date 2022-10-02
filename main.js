b5f70d18605549c8722356c8f6089aa056f46ca1
tanishqkrk <tanishqkrk@gmail.com>
2022-10-03 03:16:22 +0530

    final changes!

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
-const apiKey = 'at_Afae2z1wztB5f4gRmhht2iZPnyeG3';
-const currentIp = document.querySelector('.ip__value');
-const currentLocation = document.querySelector('.location__value');
-const currentTimeZone = document.querySelector('.timezone__value');
-const currentIsp = document.querySelector('.isp__value');
-let map = new L.map('map');
-let x = 0;
-let y = 0;
-let z = 1;
-const marker = L.marker([x, y], z);
-const enteredIp = document.querySelector('.header__input--input');
-const submitButton = document.querySelector('.header__input--button');
+const mainFunction = e => {
+    // Selectors
+    const ipAddress = document.querySelector('.header__input--input').value;
+    const ip = document.querySelector('.ip__value');
+    const location = document.querySelector('.location__value');
+    const timezone = document.querySelector('.timezone__value');
+    const isp = document.querySelector('.isp__value');
 
-map.setView([x, y], z);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    const apiKey = 'at_ptq5iCtbeeu2pV1kxSlEAZ43nLKYS';
+    const URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`;
+    console.log(URL);
+    fetch(URL).then(response => response.json())
+        .then(data => update(data))
+        .catch(data => clear(data))
 
-submitButton.addEventListener('click', e => {
-    marker.addTo(map).bindPopup(`${enteredIp.value}<br>${currentLocation.innerText}`);
-})
+    const clear = () => {
+        ip.innerHTML = '-';
+        location.innerHTML = '-'
+        timezone.innerHTML = '-'
+        isp.innerHTML = '-'
+    }
+
+    const update = (data) => {
+        console.log(data)
+        const map = L.map('map');
+        let x = data.location.lat;
+        let y = data.location.lng;
+        let z = 8;
+        map.setView([x, y], z);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map)
+        L.marker([x, y]).addTo(map)
+            .bindPopup(ipAddress)
+            .openPopup();
+
+        ip.innerHTML = ipAddress;
+        location.innerHTML = data.location.city
+        timezone.innerHTML = data.location.timezone
+        isp.innerHTML = data.isp
+    }
+}
+
+
+if (document.querySelector('.header__input').value != '') {
+    document.querySelector('.header__input--button').addEventListener('click', mainFunction)
+}
